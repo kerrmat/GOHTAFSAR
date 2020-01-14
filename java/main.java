533b3895d8eb2745211bf052d131bf7b775cfaa5
Matthew Kerr <kerrmat@oregonstate.edu>
2020-01-13 18:40:58 -0800

    Set up driver structure & added graphics window

--- java/main.java
+++ java/main.java
@@ -1,20 +1,8 @@
-import gohtafsar.*;
+import drivers.*;
 
-class main{
+public class main{
 	public static void main(String[] args){
-		Player player1 = new Player();
-		player1.status();
-		
-		System.out.println("FIRING WEAPON 10 TIMES");
-		
-		for(int i = 0; i < 10; i++)
-			player1.getLO().fire();
-		
-		player1.getLO().status();
-		
-		System.out.println("RELOADING");
-		
-		player1.getLO().reload();
-		player1.getLO().status();
+		Driver driver = new Driver();
+		driver.test();
 	}
 }
