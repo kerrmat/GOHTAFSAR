697bc2ea4296b0cda4c38bd2e20fde5258334d03
AlexanderSmith2098 <smitale2@oregonstate.edu>
2020-01-13 17:50:35 -0800

    added inline comments for player.java weapon.java

--- java/gohtafsar/Player.java
+++ java/gohtafsar/Player.java
@@ -1,16 +1,21 @@
 package gohtafsar;
 
 public class Player{
+	//player has health and shields
 	int health = 100;
 	int shields = 100;
 	
+	//coords will be for position, speed used to update coords
 	int xCoord = 0;
 	int yCoord = 0;
 	int speed = 1;
 	
+	//controls 0 will correspond to WASD, 1 to arrow keys
 	int controls = 0;
+	//weapon object
 	Weapon loadout;
 	
+	//default constructor gives player default weapon (assault rifle)
 	public Player(){
 		loadout = new Weapon();
 	}
@@ -30,6 +35,7 @@ public class Player{
 		System.out.println("CONTROLS: " + controls);
 	}
 	
+	//getters and setters
 	public Weapon getLO(){
 		return loadout;
 	}
